15f4cac7a47160985ada1b3621b892e95699e298
JasonMNVT <jason.manivant@gmail.com>
2022-12-08 15:04:07 +0100

    third commit

---
+++ src/Controller/AnnonceController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Annonce;
+use App\Form\AnnonceType;
+use App\Repository\AnnonceRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+#[Route('/annonce')]
+class AnnonceController extends AbstractController
+{
+    #[Route('/', name: 'app_annonce_index', methods: ['GET'])]
+    public function index(AnnonceRepository $annonceRepository): Response
+    {
+        return $this->render('annonce/index.html.twig', [
+            'annonces' => $annonceRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_annonce_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, AnnonceRepository $annonceRepository, SluggerInterface $slugger): Response
+    {
+        $author = $this->getUser();
+        $annonce = new Annonce();
+        $form = $this->createForm(AnnonceType::class, $annonce);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('imgfile')->getData();
+
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+                $annonce->setImgfile($newFilename);
+            }
+            $annonce->setAuthor($author);
+            $annonce->setIsVisible(true);
+            $annonceRepository->add($annonce, true);
+            return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
+        }
+        return $this->renderForm('annonce/new.html.twig', [
+            'annonce' => $annonce,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_annonce_show', methods: ['GET'])]
+    public function show(Annonce $annonce): Response
+    {
+        return $this->render('annonce/show.html.twig', [
+            'annonce' => $annonce,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_annonce_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Annonce $annonce, AnnonceRepository $annonceRepository): Response
+    {
+        $form = $this->createForm(AnnonceType::class, $annonce);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $annonceRepository->save($annonce, true);
+
+            return $this->redirectToRoute('app_annonce_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('annonce/edit.html.twig', [
+            'annonce' => $annonce,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_annonce_delete', methods: ['POST'])]
+    public function delete(Request $request, Annonce $annonce, AnnonceRepository $annonceRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $annonce->getId(), $request->request->get('_token'))) {
+            $annonceRepository->remove($annonce, true);
+        }
+
+        return $this->redirectToRoute('app_annonce_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
